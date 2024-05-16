4178ef5f2f91de705a640ad323a2032fc264966f
MichalBassa <mb4154191@gmail.com>
2024-05-16 22:48:53 +0300

    add titel add to array add parameter

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import index from 'index.html'
-const size=5;
+const sum=5;
+const count=10;
 
 console.log(size);
-
+console.log(sum*10);
 console.log(size * 7);
 
 console.log("hello");
@@ -11,7 +12,7 @@ console.log("Michal Bassa");
 
 console.log("conflict");
 
-const array=[1, 2, 3];
+const array=[1, 2, 3,4, 5 ];
 
 console.log(array);
 
@@ -20,3 +21,4 @@ console.log(index.toString());
 console.log(yehudit1);
 
 console.log("yehudit2");
+console.log("new michal bassa")
